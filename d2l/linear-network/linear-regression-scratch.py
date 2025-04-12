3de990e36a5ee73ee8a292ff389afa61eb02b4ef
MingliangLi <limingliang0527@gmail.com>
2025-04-12 15:06:05 +0800

    linear regression
    Signed-off-by: MingliangLi <limingliang0527@gmail.com>

--- d2l/linear-network/linear-regression-scratch.py
+++ d2l/linear-network/linear-regression-scratch.py
@@ -2,7 +2,6 @@ import random
 import torch
 from d2l import torch as d2l
 
-
 def synthetic_data(w, b, num_examples):
     X: torch.Tensor = torch.normal(0, 1, (num_examples, len(w)))
     y: torch.Tensor = torch.matmul(X, w) + b
@@ -27,7 +26,6 @@ for X, y in data_iter(batch_size, features, labels):
     print(X, '\n', y)
     break
 
-# 初始化模型参数
 w = torch.normal(0, 0.01, size=(2, 1), requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
 def linreg(X, w, b):
@@ -36,8 +34,7 @@ def linreg(X, w, b):
 def squared_loss(y_hat, y):
     return  (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
 
-def sgd(params, lr, batch_size):  #@save
-    """小批量随机梯度下降"""
+def sgd(params, lr, batch_size):
     with torch.no_grad():
         for param in params:
             param -= lr * param.grad / batch_size
@@ -52,7 +49,7 @@ for epoch in range(num_epochs):
     for X, y in data_iter(batch_size, features, labels):
         l = loss(net(X, w, b), y)
         l.sum().backward()
-        sgd([w, b], lr, batch_size)  # 使用参数的梯度更新参数
+        sgd([w, b], lr, batch_size)
     with torch.no_grad():
         train_l = loss(net(features, w, b), labels)
         print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
